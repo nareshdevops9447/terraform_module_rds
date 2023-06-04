076bc1571a9295c16dd206747b0f4e8ecb806021
nareshdocker9447 <nk122121@gmail.com>
2023-06-04 21:20:47 +0100

    added main.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "allocated_storage" {
+
+}
+
+variable "db_name" {
+
+}
+
+variable "identifier" {
+
+}
+
+variable "engine" {
+
+}
+
+variable "engine_version" {
+
+}
+
+variable "instance_type" {
+
+}
+
+variable "username" {
+
+}
+
+variable "db_subnet_group_name" {
+
+}
+
+variable "db_security_group_ids" {
+    type = list
+}
+
+variable "secret_name" {
+
+}
